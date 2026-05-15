0bd60465bd230b51285941921a87de7d6c075aec
PureLeach <barannikov.mu@gmail.com>
2026-05-15 23:16:49 +0300

    fix Dockerfile and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,16 +2,13 @@
 set -e
 
 echo "💤 Waiting for the database to become available..."
-until pg_isready -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -d ${POSTGRES_DB_NAME}; do
-  echo "The database is unavailable, we're waiting..."
+until pg_isready -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -d "${POSTGRES_DB_NAME}"; do
+  echo "Database is unavailable, retrying..."
   sleep 2
 done
 
+echo "📦 Applying migrations..."
+goose -dir ./migrations postgres "${DATABASE_URL}" up
 
-echo "📦 Performing migrations..."
-goose -dir ./migrations postgres ${DATABASE_URL} up
-
-echo "🚀 Launch the application..."
-./time-capsule-memories
-
-echo "🛑 The application has terminated"
+echo "🚀 Starting application..."
+exec ./time-capsule-memories
